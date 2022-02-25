dd3c4360502942ec71351d17560fe8ff6b93b2dd
yun1lie <1132264073@qq.com>
2022-02-25 14:34:37 +0800

    使用模板

--- hello.py
+++ hello.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 @app.route('/')
 def helloWorld():
     return "hello world!"
+
+@app.route('/hello')
+def hello():
+    return render_template('helloworld.html')
 
 
 app.run()
