8ce561d8cf00e27595ac5750deb34a8d8c7df14d
yun1lie <1132264073@qq.com>
2022-02-25 22:41:07 +0800

    增加操作数据库类

--- hello.py
+++ hello.py
@@ -2,34 +2,40 @@ from pickle import GET
 
 from flask import Flask, render_template, request
 
+from pp.OpDataBase import OpDataBase
+
 app = Flask(__name__)
 # 开启flask的调试模式
 app.debug = True
 
 
-@app.route('/')
+@app.route("/")
 def helloWorld():
 
     # 显示首页index.html
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/hello')
+
+@app.route("/hello")
 def hello():
 
     # hello world 页面用来测试
-    return render_template('helloworld.html')
+    return render_template("helloworld.html")
 
 
 # 插入单词
-@app.route('/insert',methods=['GET', 'POST'])
+@app.route("/insert", methods=["GET", "POST"])
 def ins():
 
     # 增加一开始判断请求语句，可以避免一上来就提交一个NONE
-    if request.method == 'POST':
-        name = request.form.get('name')
-        translate = request.form.get('translate')
+    if request.method == "POST":
+        name = request.form.get("name")
+        translate = request.form.get("translate")
         print(name, translate)
-    return  render_template('insert.html')
+    return render_template("insert.html")
 
 
-app.run()
+if __name__ == "__main__":
+    aa = OpDataBase()
+    aa.print()
+    app.run()
---
+++ pp/OpDataBase.py
@@ -0,0 +1,5 @@
+class OpDataBase:
+    def __init__(self) -> None:
+        pass
+    def print(self):
+        print('aaa')
