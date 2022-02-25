cf8f6ac0e1e025e820dceb74998f2ba47b1f9199
yun1lie <1132264073@qq.com>
2022-02-25 15:52:09 +0800

    添加主页面

--- hello.py
+++ hello.py
@@ -1,12 +1,18 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+
+
 @app.route('/')
 def helloWorld():
-    return "hello world!"
+
+    # 显示首页index.html
+    return render_template('index.html')
 
 @app.route('/hello')
 def hello():
+
+    # hello world 页面用来测试
     return render_template('helloworld.html')
 
 
