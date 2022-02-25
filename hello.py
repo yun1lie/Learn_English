dc83589c951ed54263bbba6b54d01502e1962921
yun1lie <1132264073@qq.com>
2022-02-25 16:09:33 +0800

    将要插入单词

--- hello.py
+++ hello.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+# 开启flask的调试模式
+app.debug = True
 
 
 @app.route('/')
@@ -14,6 +16,12 @@ def hello():
 
     # hello world 页面用来测试
     return render_template('helloworld.html')
+
+
+# 插入单词
+@app.route('/insert')
+def ins():
+    return  render_template('insert.html')
 
 
 app.run()
